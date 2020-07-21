5537c266d171eabdaa7599a1d40b0c1105362e78
Nivaja Ranjit <numetalxz@gmail.com>
2020-07-21 14:15:10 +0545

    started vue

--- app/Http/Resources/Article.php
+++ app/Http/Resources/Article.php
@@ -14,12 +14,8 @@ class Article extends JsonResource
      */
     public function toArray($request)
     {
-        //return parent::toArray($request);
-        return [
-            'id'=>$this->id,
-            'title'=>$this->title,
-            'body'=>$this->body            
-        ];
+        return parent::toArray($request);
+        
     }
     public function with($request){
         return [
